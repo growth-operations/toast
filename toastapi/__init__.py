d1657680a6947c05de685908a7f14cf8adf2313f
j-madrone <jasper.madrone@gmail.com>
2025-09-09 22:22:30 -0400

    Update package version to 1.0.38 across all relevant files.

--- toastapi/__init__.py
+++ toastapi/__init__.py
@@ -14,7 +14,7 @@
 """  # noqa: E501
 
 
-__version__ = "1.0.37"
+__version__ = "1.0.38"
 
 # Define package exports
 __all__ = [
--- toastapi/configuration.py
+++ toastapi/configuration.py
@@ -506,7 +506,7 @@ class Configuration:
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 1.0.0\n"\
-               "SDK Package Version: 1.0.37".\
+               "SDK Package Version: 1.0.38".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self) -> List[HostSetting]:
